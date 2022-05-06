f5f60032a959ae8546886e11c46aef4994264903
Chukwuma Okoroji <dandychux@gmail.com>
2022-05-05 22:40:13 -0400

    finished module 4 assignment

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
-let result = document.getElementById('number').innerHTML;
-const array_length = 10;
-let arr = [];
-for(let i = 0; i < array_length; i++) {
-    arr.push(Math.floor(Math.random() * 10));
-}
-for(let j = 0; j < arr.length; j++) {
-    if(arr[j] % 2 == 0) {
-    result = `${arr[j]} is even.`;
-    }
-    else {
-    result = `${arr[j]} is odd.`;
-    }
-}
+var y = 5;
+var x = ++y;
+document.getElementById('number1').innerHTML = `1st Number is: ${y}`;
+document.getElementById('number2').innerHTML = `2nd Number is: ${x}`;
